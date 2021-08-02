0a58776def8abc5a6da4727834a135731fb39a0a
sainarasimharao <srao@tapestry.com>
2021-08-02 20:12:55 +0530

    Task : fixing import issue

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from os import environ, path
 from dotenv import load_dotenv
+import redis
 
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, ".env"))
@@ -7,8 +8,8 @@ load_dotenv(path.join(basedir, ".env"))
 
 class Config:
     SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
-    FLASK_APP = "app.py"
 
     STATIC_FOLDER = "static"
     TEMPLATES_FOLDER = "templates"
